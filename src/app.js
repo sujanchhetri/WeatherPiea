e684cf8f243ca77581acfba643c0f896ebd7cecd
Sujan Chhetri <zoranhaude@gmail.com>
2020-05-07 19:07:50 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./extras/geocode.js')
 const forecast = require('./extras/forecast.js')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -100,6 +101,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is running')
+app.listen(port, () => {
+    console.log('Server is running'+port)
 })
