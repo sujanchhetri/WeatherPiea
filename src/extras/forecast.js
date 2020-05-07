581fb89a1dde7449f74772d1e3bec273cd8fed62
Sujan Chhetri <zoranhaude@gmail.com>
2020-05-08 02:20:13 +0530

    updated

--- src/extras/forecast.js
+++ src/extras/forecast.js
@@ -12,13 +12,12 @@ const forecast = (latitude,longitude,callback) => {
                                                                       var t = Math.round(body.main.temp-273.15)
                                                                       var t1 = Math.round(body.main.temp_max-273.15)
                                                                       var t2 = Math.round(body.main.temp_min-273.15)
-                                                                      let t3 = body.timezone;
                                                                       let x = body.sys.sunrise;
                                                                       let y = body.sys.sunset;
-                                                                      let date1 = new Date(x*1000).toLocaleTimeString('en-US', { t3, hour12: true });
-                                                                      let date2 = new Date(y*1000).toLocaleTimeString('en-US', {t3, hour12: true })
-                                                                      let time = new Date().toLocaleTimeString('en-US', { t3, hour12: true })
-                                                                      callback(undefined,'It is currently'+time+'.The temperature is '+t+'°C,'+body.weather[0].description+'.Maximum temperature  is '+t1+'°C & Minimum temperature  is '+t2+'°C.Sunrise at '+date1+'.Sunset at '+date2)
+                                                                      let d1 = new Date(x*1000).toLocaleTimeString();
+                                                                      let d2 = new Date(y*1000).toLocaleTimeString();
+                                                                      let time = new Date().toLocaleTimeString();
+                                                                      callback(undefined,'It is currently '+time+'.The temperature is '+t+'°C,'+body.weather[0].description+'.Maximum temperature  is '+t1+'°C & Minimum temperature  is '+t2+'°C.Sunrise at '+d1+'.Sunset at '+d2)
                                                                       }
                                    })
                                    
