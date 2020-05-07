227cdfef39d30b8842d91100cf896cb4dc0c36df
Sujan Chhetri <zoranhaude@gmail.com>
2020-05-08 00:38:06 +0530

    updated

--- src/extras/forecast.js
+++ src/extras/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude,longitude,callback) => {
-                                   const url = 'http://api.openweathermap.org/data/2.5/weather?lat='+latitude+'&lon='+longitude+'&APPID=f169210a95383e810ddabb10e36ff69d'
+                                   const url = 'http://api.openweathermap.org/data/2.5/weather?lat='+longitude+'&lon='+latitude+'&APPID=f169210a95383e810ddabb10e36ff69d' 
 
                                    request ({url,json:true},(error,{body}) => {
                                                                       if(error){
@@ -9,7 +9,14 @@ const forecast = (latitude,longitude,callback) => {
                                                                       }else if(body.error){
                                                                       callback('Unable to find location',undefined)
                                                                       }else{
-                                                                      callback(undefined,body.weather[0].description+'.It is currently '+body.main.temp+' degrees outside' )
+                                                                      var t = Math.round(body.main.temp-273.15)
+                                                                      var t1 = Math.round(body.main.temp_max-273.15)
+                                                                      var t2 = Math.round(body.main.temp_min-273.15)
+                                                                      let x = body.sys.sunrise;
+                                                                      let y = body.sys.sunset;
+                                                                      let date1 = new Date(x*1000);
+                                                                      let date2 = new Date(y*1000);
+                                                                      callback(undefined,'Current temperature is '+t+'°C,'+body.weather[0].description+'.Maximum temperature  is '+t1+'°C & Minimum temperature  is '+t2+'°C.Sunrise at '+date1.toLocaleTimeString()+'.Sunset at '+date2.toLocaleTimeString())
                                                                       }
                                    })
                                    
